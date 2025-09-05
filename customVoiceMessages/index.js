a1b44736414917c81c54fb0339fc73032790b7dc
MasterCraft78 <70117583+MasterCraft78@users.noreply.github.com>
2025-09-06 03:41:47 +0700

    Update index.js

--- customVoiceMessages/index.js
+++ customVoiceMessages/index.js
@@ -1,260 +1,175 @@
-(function (plugin, metro, patcher, vendetta, common, assets, utils, ui, components, storage) {
+(function(f, s, c, a, o, l, _, v, h, p) {
     "use strict";
 
-    // --- Utility function to modify audio attachments ---
-    function transformAudio(item) {
-        if (item?.mimeType?.startsWith("audio")) {
-            item.mimeType = "audio/ogg";
-            item.waveform = "AEtWPyUaGA4OEAcA";
-            item.durationSecs = 60;
-        }
+    function w(e) {
+        e?.mimeType?.startsWith("audio") && (e.mimeType = "audio/ogg", e.waveform = "AEtWPyUaGA4OEAcA", e.durationSecs = 60)
     }
 
-    // --- Patch uploads (local & cloud) to mark audio as voice messages ---
-    function patchUploads() {
-        const unpatches = [];
-
-        function applyPatch(name) {
-            try {
-                const target = metro.findByProps(name);
-                const unpatch = patcher.before(name, target, (args) => {
-                    const upload = args[0];
-                    if (!storage.sendAsVM || upload.flags === 8192) return;
-
-                    const item = upload.items?.[0] ?? upload;
-                    if (item?.mimeType?.startsWith("audio")) {
-                        transformAudio(item);
-                        upload.flags = 8192;
-                    }
-                });
-                unpatches.push(unpatch);
-            } catch {}
-        }
-
-        applyPatch("uploadLocalFiles");
-        applyPatch("CloudUpload");
-
-        return () => unpatches.forEach((u) => u());
-    }
-
-    // --- Patch incoming messages (LOAD_MESSAGES_SUCCESS) ---
-    function patchMessageLoad() {
-        return patcher.before(
-            "actionHandler",
-            common.FluxDispatcher._actionHandlers
-                ._computeOrderedActionHandlers("LOAD_MESSAGES_SUCCESS")
-                .find((h) => h.name === "MessageStore"),
-            (args) => {
-                if (!storage.allAsVM) return;
-
-                args[0].messages.forEach((msg) => {
-                    if (msg.flags == 8192) return;
-                    msg.attachments.forEach((att) => {
-                        if (att.content_type?.startsWith?.("audio")) {
-                            msg.flags |= 8192;
-                            att.waveform = "AEtWPyUaGA4OEAcA";
-                            att.duration_secs = 60;
-                        }
-                    });
-                });
+    function P() {
+        const e = [],
+            t = function(n) {
+                try {
+                    const u = s.findByProps(n),
+                        r = c.before(n, u, function(A) {
+                            const i = A[0];
+                            if (!a.storage.sendAsVM || i.flags === 8192) return;
+                            const m = i.items?.[0] ?? i;
+                            m?.mimeType?.startsWith("audio") && (w(m), i.flags = 8192)
+                        });
+                    e.push(r)
+                } catch {}
+            };
+        return t("uploadLocalFiles"), t("CloudUpload"),
+            function() {
+                return e.forEach(function(n) {
+                    return n()
+                })
             }
-        );
     }
 
-    // --- Patch new messages (MESSAGE_CREATE) ---
-    function patchMessageCreate() {
-        return patcher.before(
-            "actionHandler",
-            common.FluxDispatcher._actionHandlers
-                ._computeOrderedActionHandlers("MESSAGE_CREATE")
-                .find((h) => h.name === "MessageStore"),
-            (args) => {
-                if (!storage.allAsVM || args[0].message.flags == 8192) return;
-
-                let msg = args[0].message;
-                if (msg?.attachments?.[0]?.content_type?.startsWith("audio")) {
-                    msg.flags |= 8192;
-                    msg.attachments.forEach((att) => {
-                        att.waveform = "AEtWPyUaGA4OEAcA";
-                        att.duration_secs = 60;
-                    });
-                }
-            }
-        );
+    function V() {
+        return c.before("actionHandler", o.FluxDispatcher._actionHandlers._computeOrderedActionHandlers("LOAD_MESSAGES_SUCCESS").find(function(e) {
+            return e.name === "MessageStore"
+        }), function(e) {
+            a.storage.allAsVM && e[0].messages.forEach(function(t) {
+                t.flags != 8192 && t.attachments.forEach(function(n) {
+                    n.content_type?.startsWith?.("audio") && (t.flags |= 8192, n.waveform = "AEtWPyUaGA4OEAcA", n.duration_secs = 60)
+                })
+            })
+        })
     }
 
-    // --- Patch updated messages (MESSAGE_UPDATE) ---
-    function patchMessageUpdate() {
-        return patcher.before(
-            "actionHandler",
-            common.FluxDispatcher._actionHandlers
-                ._computeOrderedActionHandlers("MESSAGE_UPDATE")
-                .find((h) => h.name === "MessageStore"),
-            (args) => {
-                if (!storage.allAsVM || args[0].message.flags == 8192) return;
-
-                let msg = args[0].message;
-                if (msg?.attachments?.[0]?.content_type?.startsWith("audio")) {
-                    msg.flags |= 8192;
-                    msg.attachments.forEach((att) => {
-                        att.waveform = "AEtWPyUaGA4OEAcA";
-                        att.duration_secs = 60;
-                    });
-                }
-            }
-        );
+    function b() {
+        return c.before("actionHandler", o.FluxDispatcher._actionHandlers._computeOrderedActionHandlers("MESSAGE_CREATE").find(function(e) {
+            return e.name === "MessageStore"
+        }), function(e) {
+            if (!a.storage.allAsVM || e[0].message.flags == 8192) return;
+            let t = e[0].message;
+            t?.attachments?.[0]?.content_type?.startsWith("audio") && (t.flags |= 8192, t.attachments.forEach(function(n) {
+                n.waveform = "AEtWPyUaGA4OEAcA", n.duration_secs = 60
+            }))
+        })
     }
 
-    // --- Action sheet buttons for voice messages ---
-    const { FormRow } = components.Forms;
-    const ActionSheetRow = metro.findByProps("ActionSheetRow")?.ActionSheetRow;
+    function C() {
+        return c.before("actionHandler", o.FluxDispatcher._actionHandlers._computeOrderedActionHandlers("MESSAGE_UPDATE").find(function(e) {
+            return e.name === "MessageStore"
+        }), function(e) {
+            if (!a.storage.allAsVM || e[0].message.flags == 8192) return;
+            let t = e[0].message;
+            t?.attachments?.[0]?.content_type?.startsWith("audio") && (t.flags |= 8192, t.attachments.forEach(function(n) {
+                n.waveform = "AEtWPyUaGA4OEAcA", n.duration_secs = 60
+            }))
+        })
+    }
+    const {
+        FormRow: E
+    } = h.Forms, d = s.findByProps("ActionSheetRow")?.ActionSheetRow;
 
-    function ActionRow({ label, icon, onPress }) {
-        const styles = common.stylesheet.createThemedStyleSheet({
+    function y({
+        label: e,
+        icon: t,
+        onPress: n
+    }) {
+        const u = o.stylesheet.createThemedStyleSheet({
             iconComponent: {
                 width: 24,
                 height: 24,
-                tintColor: ui.semanticColors.INTERACTIVE_NORMAL,
-            },
+                tintColor: v.semanticColors.INTERACTIVE_NORMAL
+            }
         });
-
-        if (ActionSheetRow) {
-            return React.createElement(ActionSheetRow, {
-                label,
-                icon: React.createElement(ActionSheetRow.Icon, {
-                    source: icon,
-                    IconComponent: () =>
-                        React.createElement(common.ReactNative.Image, {
-                            resizeMode: "cover",
-                            style: styles.iconComponent,
-                            source: icon,
-                        }),
-                }),
-                onPress: () => onPress?.(),
-            });
-        } else {
-            return React.createElement(FormRow, {
-                label,
-                leading: React.createElement(FormRow.Icon, { source: icon }),
-                onPress: () => onPress?.(),
-            });
-        }
-    }
-
-    // --- Patch long press menu (add download & copy buttons) ---
-    const ActionSheet = metro.findByProps("openLazy", "hideActionSheet");
-
-    function patchActionSheet() {
-        return patcher.before("openLazy", ActionSheet, (args) => {
-            const [promise, name, opts] = args;
-            const message = opts?.message;
-
-            if (name !== "MessageLongPressActionSheet" || !message) return;
-
-            promise.then((module) => {
-                const unpatch = patcher.after("default", module, (res, comp) => {
-                    common.React.useEffect(() => () => unpatch(), []);
-                    const buttons = utils.findInReactTree(comp, (x) =>
-                        x?.[0]?.type?.name === "ButtonRow"
-                    );
-                    if (!buttons) return comp;
-
-                    if (message.hasFlag(8192)) {
-                        // Download option
-                        buttons.splice(
-                            5,
-                            0,
-                            React.createElement(ActionRow, {
-                                label: "Download Voice Message",
-                                icon: assets.getAssetIDByName("ic_download_24px"),
-                                onPress: async () => {
-                                    await metro
-                                        .findByProps("downloadMediaAsset")
-                                        .downloadMediaAsset(message.attachments[0].url, 0);
-                                    metro.findByProps("hideActionSheet").hideActionSheet();
-                                },
-                            })
-                        );
-
-                        // Copy URL option
-                        buttons.splice(
-                            6,
-                            0,
-                            React.createElement(ActionRow, {
-                                label: "Copy Voice Message URL",
-                                icon: assets.getAssetIDByName("copy"),
-                                onPress: async () => {
-                                    common.clipboard.setString(
-                                        message.attachments[0].url
-                                    );
-                                    metro.findByProps("hideActionSheet").hideActionSheet();
-                                },
-                            })
-                        );
-                    }
-                });
-            });
-        });
-    }
-
-    // --- Settings UI ---
-    const { FormDivider, FormIcon, FormSwitchRow } = components.Forms;
-
-    function Settings() {
-        ui.useProxy(storage);
-
-        return React.createElement(
-            common.ReactNative.ScrollView,
-            null,
-            React.createElement(FormSwitchRow, {
-                label: "Send audio files as Voice Message",
-                leading: React.createElement(FormIcon, {
-                    source: assets.getAssetIDByName("voice_bar_mute_off"),
-                }),
-                onValueChange: (v) => (storage.sendAsVM = v),
-                value: storage.sendAsVM,
+        return d ? React.createElement(d, {
+            label: e,
+            icon: React.createElement(d.Icon, {
+                source: t,
+                IconComponent: function() {
+                    return React.createElement(o.ReactNative.Image, {
+                        resizeMode: "cover",
+                        style: u.iconComponent,
+                        source: t
+                    })
+                }
             }),
-            React.createElement(FormDivider, null),
-            React.createElement(FormSwitchRow, {
-                label: "Show every audio file as a Voice Message",
-                leading: React.createElement(FormIcon, {
-                    source: assets.getAssetIDByName("ic_stage_music"),
-                }),
-                onValueChange: (v) => (storage.allAsVM = v),
-                value: storage.allAsVM,
-            })
-        );
+            onPress: function() {
+                return n?.()
+            }
+        }) : React.createElement(E, {
+            label: e,
+            leading: React.createElement(E.Icon, {
+                source: t
+            }),
+            onPress: function() {
+                return n?.()
+            }
+        })
     }
+    const I = s.findByProps("openLazy", "hideActionSheet");
 
-    // --- Default storage values ---
-    storage.sendAsVM ??= true;
-    storage.allAsVM ??= false;
+    function B() {
+        return c.before("openLazy", I, function(e) {
+            const [t, n, u] = e, r = u?.message;
+            n !== "MessageLongPressActionSheet" || !r || t.then(function(A) {
+                const i = c.after("default", A, function(m, M) {
+                    o.React.useEffect(function() {
+                        return function() {
+                            i()
+                        }
+                    }, []);
+                    const g = _.findInReactTree(M, function(O) {
+                        return O?.[0]?.type?.name === "ButtonRow"
+                    });
+                    if (!g) return M;
+                    r.hasFlag(8192) && (g.splice(5, 0, o.React.createElement(y, {
+                        label: "Download Voice Message",
+                        icon: l.getAssetIDByName("ic_download_24px"),
+                        onPress: async function() {
+                            await s.findByProps("downloadMediaAsset").downloadMediaAsset(r.attachments[0].url, 0), s.findByProps("hideActionSheet").hideActionSheet()
+                        }
+                    })), g.splice(6, 0, o.React.createElement(y, {
+                        label: "Copy Voice Message URL",
+                        icon: l.getAssetIDByName("copy"),
+                        onPress: async function() {
+                            o.clipboard.setString(r.attachments[0].url), s.findByProps("hideActionSheet").hideActionSheet()
+                        }
+                    })))
+                })
+            })
+        })
+    }
+    const {
+        FormDivider: D,
+        FormIcon: S,
+        FormSwitchRow: R
+    } = h.Forms;
 
-    // --- Initialize patches ---
-    const patches = [
-        patchUploads(),
-        patchMessageCreate(),
-        patchMessageLoad(),
-        patchMessageUpdate(),
-        patchActionSheet(),
-    ];
+    function F() {
+        return p.useProxy(a.storage), React.createElement(o.ReactNative.ScrollView, null, React.createElement(R, {
+            label: "Send audio files as Voice Message",
+            leading: React.createElement(S, {
+                source: l.getAssetIDByName("voice_bar_mute_off")
+            }),
+            onValueChange: function(e) {
+                return a.storage.sendAsVM = e
+            },
+            value: a.storage.sendAsVM
+        }), React.createElement(D, null), React.createElement(R, {
+            label: "Show every audio file as a Voice Message",
+            leading: React.createElement(S, {
+                source: l.getAssetIDByName("ic_stage_music")
+            }),
+            onValueChange: function(e) {
+                return a.storage.allAsVM = e
+            },
+            value: a.storage.allAsVM
+        }))
+    }
+    a.storage.sendAsVM ??= !0, a.storage.allAsVM ??= !1;
+    const U = [P(), b(), V(), C(), B()],
+        H = function() {
+            U.forEach(function(e) {
+                return e()
+            })
+        };
+    return f.onUnload = H, f.settings = F, f
+})({}, vendetta.metro, vendetta.patcher, vendetta.plugin, vendetta.metro.common, vendetta.ui.assets, vendetta.utils, vendetta.ui, vendetta.ui.components, vendetta.storage);
 
-    const onUnload = () => patches.forEach((u) => u());
-
-    // --- Export plugin ---
-    plugin.onUnload = onUnload;
-    plugin.settings = Settings;
-
-    return plugin;
-})(
-    {},
-    vendetta.metro,
-    vendetta.patcher,
-    vendetta.plugin,
-    vendetta.metro.common,
-    vendetta.ui.assets,
-    vendetta.utils,
-    vendetta.ui,
-    vendetta.ui.components,
-    vendetta.storage
-);
